44907cefcfd4f6b275270e6d73c5126939598460
Sounak Pradhan <22390515+sounak98@users.noreply.github.com>
2024-07-08 23:37:48 +0200

    chore: more cleanup

--- src/app/table.tsx
+++ src/app/table.tsx
@@ -5,8 +5,8 @@ export default function CollateralTable() {
     <div className="mt-4 border border-white border-opacity-5 rounded overflow-hidden bg-[rgba(20,24,33,0.6)]">
       <table className="w-full text-center text-sm table-fixed divide-y divide-white divide-opacity-5">
         <thead className="h-10">
-          <tr className="">
-            <td className="" />
+          <tr className="font-semibold">
+            <td />
             {activePlatforms.map((platform, i) => (
               <td key={i} className="">
                 {platform}
@@ -20,9 +20,9 @@ export default function CollateralTable() {
               key={i}
               className="h-10 odd:bg-[rgba(29,31,40,0.2)] even:bg-[rgba(27,35,48,0.2)]"
             >
-              <td className="">{collateral}</td>
+              <td className="font-semibold">{collateral}</td>
               {activePlatforms.map((platform, j) => (
-                <td key={j} className="">
+                <td key={j}>
                   $
                   {Intl.NumberFormat("en-US", {
                     notation: "compact",
